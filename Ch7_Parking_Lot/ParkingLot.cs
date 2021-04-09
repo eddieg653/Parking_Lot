cc9764f90dd7f21314614aa7a7a3e7ae9bfc97ce
Eddie Garcia <eddieg031@gmail.com>
2021-04-08 22:57:19 -0500

    Add project files.

---
+++ Ch7_Parking_Lot/ParkingLot.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ch7_Parking_Lot
+{
+    public class ParkingLot
+    {
+        //fields
+        private int mysmallcar;
+        private int mymediumcar;
+        private int mylargecar;
+
+        //Constructor
+        public ParkingLot(int mysmallcar, int mymediumcar, int mylargecar) // integer entered in parameter will become value of size
+        {
+            this.mysmallcar = mysmallcar;
+            this.mymediumcar = mymediumcar;
+            this.mylargecar = mylargecar;
+        }
+
+        public bool AddCar(string carType)
+        {
+            carType = carType.ToLower();
+            bool myparkingspace = false; //value to return to AddCar
+            if(carType == "small" && mysmallcar > 0)
+            {
+                myparkingspace = true;//changes bool value to true and returns value;
+                --mysmallcar;
+            }
+            else if(carType == "medium" && mymediumcar > 0)
+            {
+                myparkingspace = true;
+                --mymediumcar;
+            }
+            else if (carType == "large" && mylargecar > 0)
+            {
+                myparkingspace = true;
+                --mylargecar;
+            }
+            return myparkingspace;
+        }
+    }
+}
---
+++ Ch7_Parking_Lot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ch7_Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParkingLot p1 = new ParkingLot(1, 2, 3);
+            // 1 true & 3 false
+            Console.WriteLine(p1.AddCar("Small"));
+            Console.WriteLine(p1.AddCar("small"));
+            Console.WriteLine(p1.AddCar("small"));
+            Console.WriteLine(p1.AddCar("small"));
+
+            // 2 true & 2 false
+            Console.WriteLine(p1.AddCar("Medium"));
+            Console.WriteLine(p1.AddCar("medium"));
+            Console.WriteLine(p1.AddCar("medium"));
+            Console.WriteLine(p1.AddCar("medium"));
+
+            //3 true & 1 false
+            Console.WriteLine(p1.AddCar("Large"));
+            Console.WriteLine(p1.AddCar("large"));
+            Console.WriteLine(p1.AddCar("Large"));
+            Console.WriteLine(p1.AddCar("Large"));
+        }
+    }
+}
